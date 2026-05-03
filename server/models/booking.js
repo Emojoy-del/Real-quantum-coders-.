d4b26aeeaf818fcdaee6c71c3f316334716bdb5b
kimberly wambui <kimberlywamboi8@gmail.com>
2026-05-02 19:59:59 -0700

    add files

---
+++ server/models/booking.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
